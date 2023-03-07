59ec7e2489327e43d8a9cec3771bc08cfe6091ee
Ibrahim <iimran@greenlightiot.com>
2023-03-06 19:14:57 -0500

    Storing course data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScheduleModule } from '@nestjs/schedule';
+import { CourseDataService } from './services/course-data/course-data.service';
+import { CourseDataController } from './controllers/course-data.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ScheduleModule.forRoot()],
+  controllers: [AppController, CourseDataController],
+  providers: [AppService, CourseDataService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  });
+
   await app.listen(3000);
 }
 bootstrap();
