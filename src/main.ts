949871bc3a834adf18b55379c2a96d55e5a91eeb
Ibrahim <iimran@greenlightiot.com>
2023-03-10 18:51:24 -0500

    Cors origin as develop client

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'https://developbullscourses.netlify.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
 
