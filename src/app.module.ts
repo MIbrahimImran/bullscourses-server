7b1e4bab374f442ea118b0f0e76ba379974626fd
Ibrahim <iimran@greenlightiot.com>
2023-03-09 19:20:30 -0500

    Mongodb connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import { CourseDataService } from './services/course-data/course-data.service';
 import { CourseDataController } from './controllers/course-data.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { db } from '../config/local.json';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [ScheduleModule.forRoot(), MongooseModule.forRoot(db.hostUri)],
   controllers: [AppController, CourseDataController],
   providers: [AppService, CourseDataService],
 })
