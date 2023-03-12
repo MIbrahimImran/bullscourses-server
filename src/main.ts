f07997ab14875bfb87296734cb4c22f3cdd7299b
Ibrahim <iimran@greenlightiot.com>
2023-03-12 13:25:11 -0400

    CORS

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'http://localhost:4200',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  });
+  app.enableCors();
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
