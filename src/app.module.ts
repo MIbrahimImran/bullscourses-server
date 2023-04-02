4236c6412969a76bf217f64beac9e2913cfd4e8c
Ibrahim <iimran@greenlightiot.com>
2023-04-01 22:33:26 -0400

    Updated Schema name

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { CourseDataController } from './controllers/course-data.controller';
 import { SubscriptionController } from './controllers/subscription.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { db } from '../config/local.json';
-import { User, UserSchema } from './schemas/subscription.schema';
+import { User, UserSchema } from './schemas/user.schema';
 import { SubscriptionService } from './services/subscription.service';
 import { EmailService } from './services/email.service';
 import { CourseDataService } from './services/course.service';
